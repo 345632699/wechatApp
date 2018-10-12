3abd4956b01d3a4b21597a7f14acf29d69adfdc8
caicai <1144164505@qq.com>
2018-10-12 11:36:29 +0800

    删除没有用的文件

--- src/utils/tip.js
+++ src/utils/tip.js
@@ -74,7 +74,7 @@ export default class Tips {
   static alert(title) {
     wx.showToast({
       title: title,
-      image: "../images/alert.png",
+      image: "../images/icons/alert.png",
       mask: true,
       duration: 1500
     });
@@ -87,7 +87,7 @@ export default class Tips {
   static error(title, onHide) {
     wx.showToast({
       title: title,
-      image: "../images/error.png",
+      image: "../images/icons/error.png",
       mask: true,
       duration: 500
     });
